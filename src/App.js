6536ef918beff8ef502c62d0b84f8592108f7c69
sambhu513 <sambhubaburaj513@gmail.com>
2023-01-06 14:26:27 +0530

    netflix

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import NavBar from './Components/NavBar/NavBar';
+import RowPost from './Components/RowPost/RowPost';
+import {originals,action} from './url'
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Banner/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={action} title='Action' isSmall/>
+
+    </div>
+  );
+}
+
+export default App;
